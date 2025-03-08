3e5ba16d5fc2805382a3d83ce5d0b6572db65716
Allen7D <462870781@qq.com>
2025-03-08 12:45:27 +0800

    feat: 学习「配置顶部按钮 Header Buttons」

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,10 +1,12 @@
 import { Button, View, Text } from "react-native";
 import { StackActions } from "@react-navigation/native";
-import { useNavigation } from "expo-router";
+import { Stack, useNavigation } from "expo-router";
+import { useState } from "react";
 
 export default function SettingsScreen() {
   const navigation = useNavigation();
-  
+  const [count, setCount] = useState(0);
+
   const handlePopToTop = () => {
     const action = StackActions.popToTop();
     navigation.dispatch(action);
@@ -12,8 +14,16 @@ export default function SettingsScreen() {
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-	  <Text>这里是设置页</Text>
-	  <Button title="直接回到首页" onPress={handlePopToTop} />
-	</View>
+      <Stack.Screen
+        options={{
+          title: '自定义Title',
+          headerLeft: () => (<Button title="弹框" onPress={() => alert('点击了弹框')} />),
+          headerRight: () => (<Button title="计数器+1" onPress={() => setCount(count + 1)} />),
+        }}
+      />
+      <Text>这里是设置页</Text>
+      <Text>计数器: {count}</Text>
+      <Button title="直接回到首页" onPress={handlePopToTop} />
+    </View>
   )
 }
