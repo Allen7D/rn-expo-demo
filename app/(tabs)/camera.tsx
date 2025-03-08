2c4068e9cba110cbf11efa05c6f6b87314db0149
Allen7D <462870781@qq.com>
2025-03-08 22:18:27 +0800

    feat: 学习「处理平台差异」，关于截图功能的实现

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,10 +1,11 @@
 import { useState, useRef } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { type ImageSource } from 'expo-image';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
 import { ImageViewer } from '@/components/ImageViewer';
 import { CameraButton } from '@/components/CameraButton';
@@ -43,19 +44,37 @@ export default function CameraScreen() {
 	};
 
 	const onSaveImageAsync = async () => {
-		try {
-			// 屏幕截图
-			const localUri = await captureRef(imageRef, {
-				height: 440,
-				quality: 1,
-			})
-
-			await MediaLibrary.saveToLibraryAsync(localUri);
-			if (localUri) {
-				alert('Image saved successfully');
+		if (Platform.OS !== 'web') {
+			try {
+				// 屏幕截图
+				const localUri = await captureRef(imageRef, {
+					height: 440,
+					quality: 1,
+				})
+	
+				await MediaLibrary.saveToLibraryAsync(localUri);
+				if (localUri) {
+					alert('Image saved successfully');
+				}
+			} catch (e) {
+				console.log(e);
 			}
-		} catch (e) {
-			console.log(e);
+		} else {
+			// Web 端截图
+			try {
+				const dataUrl = await domtoimage.toJpeg(imageRef.current as unknown as Node, {
+					quality: 0.95,
+					width: 320,
+					height: 440,
+				})
+
+				let link = document.createElement('a');
+				link.download = 'sticker-smash.jpeg';
+				link.href = dataUrl;
+				link.click();
+			} catch (e) {
+				console.log(e);
+			}
 		}
 	}
 	const onPickImageAsync = async () => {
