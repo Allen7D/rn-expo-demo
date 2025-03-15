d300a27246e75137439b3343170a17caa8e3667c
Allen7D <462870781@qq.com>
2025-03-15 12:10:00 +0800

    feat:新增 read-detail 空白页

---
+++ app/read-detail.tsx
@@ -0,0 +1,14 @@
+import { Text, View, Button, Image, StyleSheet, Platform } from 'react-native';
+import { Link, router } from 'expo-router';
+
+export default function ReadDetailScreen() {
+  return (
+    <View>
+      <Text>ReadDetail</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
